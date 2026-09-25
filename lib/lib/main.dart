b538186396f528d416f56202191566d0d5eb7dff
rasulovxumoyun4-svg <rasulovxumoyun4@gmail.com>
2026-09-25 19:18:09 +0500

    Update main.dart

--- lib/lib/main.dart
+++ lib/lib/main.dart
@@ -1,6 +1,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:image_picker/image_picker.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
